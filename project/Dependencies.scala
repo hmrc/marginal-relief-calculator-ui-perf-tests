6db2a563e3e175129434b596c10204c847472612
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-05-30 14:24:10 +0100

    DLS-10273: Removes unneeded gatling dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,7 @@ import sbt._
 
 object Dependencies {
 
-//  TODO: UPGRADING CAUSES ERROR - NEED TO INVESTIGATE
-  private val gatlingVersion = "3.6.1"
-
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.4.3"        % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.0.0"        % Test,
-    "io.gatling"            % "gatling-test-framework"    % gatlingVersion % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion % Test
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.0.0" % Test
   )
 }
